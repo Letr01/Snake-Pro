231136611b6a26b47aa0f2e39dada54426fffe7f
Letr01 <nikita.jagudin@mail.ru>
2022-01-24 16:04:48 +0200

    Добавьте файлы проекта.

---
+++ Snake Pro/ProgramBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Snake
+{
+    internal class ProgramBase
+    {
+
+        private static void ClearConsole(int screenwidth, int screenheight)
+        {
+            var blackLine = string.Join("", new byte[screenwidth - 2].Select(b => " ").ToArray());
+            Console.ForegroundColor = ConsoleColor.Black;
+            for (int i = 1; i < screenheight - 1; i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write(blackLine);
+            }
+        }
+    }
+}
